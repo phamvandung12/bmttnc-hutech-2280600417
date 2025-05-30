f139141d53522b2b3d16c58745803dec2b8087a8
phạm văn dũng <phamvandung@Dungpham.local>
2025-05-30 12:51:23 +0700

    [add] lab-03 ui ecc

--- lab-03/cipher/ecc/ecc_cipher.py
+++ lab-03/cipher/ecc/ecc_cipher.py
@@ -1,7 +1,5 @@
-import ecdsa
-import os
+import ecdsa, os
 
-# Tạo thư mục chứa key nếu chưa có
 if not os.path.exists('cipher/ecc/keys'):
     os.makedirs('cipher/ecc/keys')
 
@@ -10,37 +8,32 @@ class ECCCipher:
         pass
 
     def generate_keys(self):
-        # Tạo private key và public key
-        sk = ecdsa.SigningKey.generate()
-        vk = sk.get_verifying_key()
-        # Lưu private key
+        sk = ecdsa.SigningKey.generate()  # Tạo khóa riêng tư
+        vk = sk.get_verifying_key()  # Lấy khóa công khai từ khóa riêng tư
+
         with open('cipher/ecc/keys/privateKey.pem', 'wb') as p:
             p.write(sk.to_pem())
-        # Lưu public key
+
         with open('cipher/ecc/keys/publicKey.pem', 'wb') as p:
             p.write(vk.to_pem())
 
     def load_keys(self):
-        # Load public key
-        with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
-            vk = ecdsa.VerifyingKey.from_pem(p.read())
-        # Load private key
         with open('cipher/ecc/keys/privateKey.pem', 'rb') as p:
             sk = ecdsa.SigningKey.from_pem(p.read())
-        return vk, sk   # <-- TRẢ VỀ (VerifyingKey, SigningKey)
 
-    def sign(self, message, key=None):
-        # Ký dữ liệu bằng private key
-        if key is None:
-            _, sk = self.load_keys()
-            key = sk
+        with open('cipher/ecc/keys/publicKey.pem', 'rb') as p:
+            vk = ecdsa.VerifyingKey.from_pem(p.read())
+
+        return sk, vk
+
+    def sign(self, message, key):
+        # Ký dữ liệu bằng khóa riêng tư
         return key.sign(message.encode('ascii'))
 
-    def verify(self, message, signature, verifying_key=None):
-        # Xác thực chữ ký bằng public key
-        if verifying_key is None:
-            verifying_key, _ = self.load_keys()
+    def verify(self, message, signature, key):
+        vk = self.load_keys()[1]  # Lấy khóa công khai
         try:
-            return verifying_key.verify(signature, message.encode('ascii'))
+            vk.verify(signature, message.encode('ascii'))
+            return True
         except ecdsa.BadSignatureError:
             return False
