540676665e3fa66198120eb387cc7e1554e6473b
phạm văn dũng <phamvandung@Dungpham.local>
2025-06-13 14:39:51 +0700

    [add] lab-05 img-hidden

--- lab-05/img-hidden/decrypt.py
+++ lab-05/img-hidden/decrypt.py
@@ -5,27 +5,23 @@ def decode_image(encoded_image_path):
     img = Image.open(encoded_image_path)
     width, height = img.size
     binary_message = ""
-
     for row in range(height):
         for col in range(width):
             pixel = img.getpixel((col, row))
             for color_channel in range(3):
                 binary_message += format(pixel[color_channel], '08b')[-1]
-
     message = ""
     for i in range(0, len(binary_message), 8):
         char = chr(int(binary_message[i:i+8], 2))
-        if char == '\0':  # Kết thúc thông điệp khi gặp dấu '\0'
+        if char == '\0':  # Kết thúc thông điệp khi gặp ký tự null
             break
         message += char
-
     return message
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: python decrypt.py <encoded_image_path>")
         return
-
     encoded_image_path = sys.argv[1]
     decoded_message = decode_image(encoded_image_path)
     print("Decoded message:", decoded_message)
